d098b0842576f49174e0f033e8a9dcfa854e661a
1yne <tnambiar2008@gmail.com>
2024-12-04 22:06:49 +0530

    Rudimentary animation

---
+++ src/lib/components/SplitText.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	let { text } = $props();
+
+	let splitText = text.split('');
+</script>
+
+<div class="cursor-default splitText">
+	{#each splitText as textChar, i}
+		<span
+			class="font-nexusbold text-[40rem] leading-none opacity-0"
+			style={`animation-delay: ${1000 + i * 100}ms`}>{textChar}</span
+		>
+	{/each}
+</div>
+
+<style>
+	@keyframes move-text {
+		0% {
+			bottom: -0.3em;
+			opacity: 0;
+		}
+
+		100% {
+			bottom: 0;
+			opacity: 1;
+		}
+	}
+
+	@keyframes scale-text-down {
+		0% {
+			scale: 1;
+		}
+
+		100% {
+			scale: 0;
+      display: none;
+		}
+	}
+
+	span {
+		animation: move-text 0.5s forwards;
+		position: relative; /* <- can be commented out to make this effect a fade-in */
+	}
+  
+  .splitText {
+    animation: scale-text-down 0.5s forwards;
+    animation-delay: 3500ms;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import * as Card from "$lib/components/ui/card";
+  import SplitText from "$lib/components/SplitText.svelte";
+</script>
+
+
+<div class="flex w-full justify-center h-screen items-center hero">
+  <SplitText text="RoMUN 2025" />
+</div>
+<div class="p-12">
+  <Card.Root>
+    <Card.Header>
+      <Card.Title>Register</Card.Title>
+      <Card.Description>Card Description</Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <p>content</p>
+    </Card.Content>
+  </Card.Root>
+</div>
+
+<style>
+  @keyframes scale-text-down {
+		0% {
+			scale: 1;
+		}
+
+		100% {
+			scale: 0;
+      display: none;
+		}
+	}
+  
+  .hero {
+    animation: scale-text-down 1s forwards;
+    animation-delay: 3500ms;
+  }
+</style>
