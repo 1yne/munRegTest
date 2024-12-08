37169b11488cf400d26359a01b8f4fa6122f1a40
1yne <tnambiar2008@gmail.com>
2024-12-08 22:14:33 +0530

    Move loading text to separate component

---
+++ src/lib/components/Loading.svelte
@@ -0,0 +1,27 @@
+<div class="p-12">
+  <h1 class="loading font-mortend">Loading</h1>
+</div>
+
+<style>
+	.loading:after {
+		overflow: hidden;
+		display: inline-block;
+		vertical-align: bottom;
+		-webkit-animation: ellipsis steps(4, end) 900ms infinite;
+		animation: ellipsis steps(4, end) 900ms infinite;
+		content: '\2026';
+		width: 0px;
+	}
+
+	@keyframes ellipsis {
+		to {
+			width: 1.25em;
+		}
+	}
+
+	@-webkit-keyframes ellipsis {
+		to {
+			width: 1.25em;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import SplitText from '$lib/components/SplitText.svelte';
+	import Loading from '$lib/components/Loading.svelte';
 	import { onMount } from 'svelte';
 	import { transitions } from '$lib/transitions';
 
@@ -17,9 +18,7 @@
 <div class={`flex h-screen w-full ${loading ? 'items-end' : 'items-center justify-center'}`}>
 	<div class="heroText px-4 rounded-lg">
 		{#if loading}
-			<div class="p-12">
-				<h1 class="loading font-mortend">Loading</h1>
-			</div>
+			<Loading />
 		{:else}
 			<div>
 				<SplitText text="RoMUN 2025" />
@@ -27,27 +26,3 @@
 		{/if}
 	</div>
 </div>
-
-<style>
-	.loading:after {
-		overflow: hidden;
-		display: inline-block;
-		vertical-align: bottom;
-		-webkit-animation: ellipsis steps(4, end) 900ms infinite;
-		animation: ellipsis steps(4, end) 900ms infinite;
-		content: '\2026';
-		width: 0px;
-	}
-
-	@keyframes ellipsis {
-		to {
-			width: 1.25em;
-		}
-	}
-
-	@-webkit-keyframes ellipsis {
-		to {
-			width: 1.25em;
-		}
-	}
-</style>
