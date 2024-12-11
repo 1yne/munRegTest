61aafeb4d548fb706148eadc3467a0aa31d80d5f
1yne <tnambiar2008@gmail.com>
2024-12-11 12:53:00 +0530

    Expand search on focus

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,8 +6,6 @@
 	function focus(el: HTMLElement) {
 		el.focus();
 	}
-
-	console.log(hover);
 </script>
 
 <div class="fixed bottom-0 flex w-full justify-center py-8">
@@ -15,17 +13,19 @@
 		onclick={() => (active = true)}
 		onmouseenter={() => (hover = true)}
 		onmouseleave={() => (hover = false)}
-		class={`searchBorder flex h-10 items-center gap-4 rounded-3xl border border-white/50 py-2 px-6 transition-all hover:border-[#D60202]`}
+		class={`searchBorder flex h-12 items-center rounded-3xl border border-white/50 px-6 py-2 transition-all hover:border-[#D60202] ${active ? '!items-end !px-0 !py-4 !pb-0' : ''}`}
 	>
-		<Search class="text-white/50 transition-all" />
-		{#if active}
-			<input
-				type="text"
-				use:focus
-				onfocusout={() => (setTimeout(() => active = false, 250))}
-				class={`searchInput bg-transparent font-montserrat text-white/50 caret-white/50 transition-all focus:outline-none ${hover ? '!text-white !caret-[#D60202]' : ''}`}
-			/>
-		{/if}
+		<div class={`flex items-center gap-4 ${active ? 'border-t border-white/50 px-6 py-3' : ''}`}>
+			<Search class={`text-white/50 transition-all ${hover ? '!text-[#d60202]' : ''}`} />
+			{#if active}
+				<input
+					type="text"
+					use:focus
+					onfocusout={() => setTimeout(() => (active = false), 250)}
+					class={`searchInput bg-transparent font-montserrat text-white/50 caret-white/50 transition-all focus:outline-none ${hover ? '!text-white !caret-[#D60202]' : ''}`}
+				/>
+			{/if}
+		</div>
 	</button>
 </div>
 
@@ -39,6 +39,22 @@
 		}
 		100% {
 			width: 12rem;
+		}
+	}
+	@keyframes animateHeightIn {
+		0% {
+			height: 3rem;
+		}
+		100% {
+			height: 12rem;
+		}
+	}
+	@keyframes animateHeightOut {
+		0% {
+			height: 12rem;
+		}
+		100% {
+			height: 3rem;
 		}
 	}
 
@@ -56,5 +72,11 @@
 	}
 	.searchInput:not(.searchInput:focus) {
 		animation: animateWidthOut 0.25s forwards;
+	}
+	.searchBorder:has(.searchInput:focus) {
+		animation: animateHeightIn 0.25s forwards;
+	}
+	.searchBorder:has(.searchInput:not(.searchInput:focus)) {
+		animation: animateHeightOut 0.25s forwards;
 	}
 </style>
