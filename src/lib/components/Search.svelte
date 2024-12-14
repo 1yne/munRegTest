0d4f6a75bbdd5bce639dafa355ad27e517c5192f
1yne <tnambiar2008@gmail.com>
2024-12-14 18:12:06 +0530

    Move Search to separate component

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import Search from 'carbon-icons-svelte/lib/Search.svelte';
+	let active = false;
+	let hover = false;
+
+	function focus(el: HTMLElement) {
+		el.focus();
+	}
+</script>
+
+<button
+	onclick={() => (active = true)}
+	onmouseenter={() => (hover = true)}
+	onmouseleave={() => (hover = false)}
+	class={`searchBorder flex h-12 items-center rounded-3xl border border-white/35 px-6 py-2 transition-all hover:border-[#D60202] ${active ? '!items-end !border-white !px-0 !py-4 !pb-0' : ''}`}
+>
+	<div class={`flex items-center gap-4 ${active ? 'border-t border-white/50 px-6 py-3' : ''}`}>
+		<Search class={`text-white/35 transition-all ${hover || active ? '!text-[#d60202]' : ''}`} />
+		{#if active}
+			<input
+				type="text"
+				use:focus
+				onfocusout={() => setTimeout(() => (active = false), 250)}
+				class={`searchInput bg-transparent font-montserrat text-white caret-[#D60202] transition-all focus:outline-none`}
+			/>
+		{/if}
+	</div>
+</button>
