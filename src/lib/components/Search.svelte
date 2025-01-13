c6e81e25e06c8eb331366ba1ceb1824dd8140d8f
1yne <tnambiar2008@gmail.com>
2025-01-13 21:57:42 +0530

    Update Search with results

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,36 +1,73 @@
 <script lang="ts">
 	import Search from 'carbon-icons-svelte/lib/Search.svelte';
 	import { store as searchStore } from '$lib/stores/store';
-	let active = false;
-	let hover = false;
+	import searchData, { type SearchData } from './searchData';
+	import { slide } from 'svelte/transition';
+
+	let active = $state(false),
+		hover = $state(false),
+		searchValue = $state(''),
+		searchResults: SearchData[] = $state([]);
 
 	function focus(el: HTMLElement) {
 		el.focus();
 	}
+
+	function searchValues() {
+		if (searchValue) {
+			searchResults = searchData.filter((val) =>
+				val.name.toLocaleLowerCase().includes(searchValue)
+			);
+		} else {
+			searchResults = [];
+		}
+	}
 </script>
 
-<button
-	onclick={() => {
-		active = true;
-		$searchStore.searchActive = true;
-	}}
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div
+	class="transition-all] flex flex-col rounded-3xl border border-white/50 hover:border-[#D60202]"
 	onmouseenter={() => (hover = true)}
 	onmouseleave={() => (hover = false)}
-	class={`searchBorder flex h-12 items-center rounded-3xl border border-white/50 px-6 py-2 transition-all hover:border-[#D60202] ${active ? '!items-end !border-white !px-0 !py-4 !pb-0' : ''}`}
 >
-	<div class={`flex items-center gap-4 ${active ? 'border-t border-white/50 px-6 py-3' : ''}`}>
+	{#if searchResults}
+		<div transition:slide|global class="w-full overflow-scroll rounded-t-3xl">
+			{#each searchResults as searchResult}
+				<button
+					transition:slide
+					class="flex w-full flex-col items-start border-b border-white/50 px-6 py-2"
+				>
+					<h1 class="text-xl font-black">{searchResult.name}</h1>
+					<p class="text-sm font-extralight">Lorem ipsum dolor sit amet consectetur.</p>
+				</button>
+			{/each}
+		</div>
+	{/if}
+	<button
+		class="flex h-12 w-full items-center justify-center gap-4 px-6 py-4"
+		onclick={() => {
+			active = true;
+			$searchStore.searchActive = true;
+		}}
+	>
 		<Search class={`text-white/50 transition-all ${hover || active ? '!text-[#d60202]' : ''}`} />
 		{#if active}
 			<input
 				type="text"
 				use:focus
-				onfocusout={() =>
+				bind:value={searchValue}
+				oninput={searchValues}
+				onfocusout={() => {
 					setTimeout(() => {
 						active = false;
 						$searchStore.searchActive = false;
-					}, 250)}
-				class={`searchInput bg-transparent font-montserrat text-white caret-[#D60202] transition-all focus:outline-none`}
+						searchResults = [];
+						searchValue = '';
+					}, 250);
+				}}
+				class="searchInput h-4 bg-transparent font-montserrat text-white caret-[#D60202] transition-all focus:outline-none"
 			/>
 		{/if}
-	</div>
-</button>
+	</button>
+</div>
