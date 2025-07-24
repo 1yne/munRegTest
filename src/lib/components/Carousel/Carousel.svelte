7c21bf44ba4265cf4f6e40c41485a10354010aac
1yne <tnambiar2008@gmail.com>
2025-07-24 23:09:24 +0530

    Add moving functionality to carousel

--- src/lib/components/Carousel/Carousel.svelte
+++ src/lib/components/Carousel/Carousel.svelte
@@ -1,31 +1,60 @@
 <script lang="ts">
+	import ChevronDown from "carbon-icons-svelte/lib/ChevronDown.svelte";
+	import ChevronUp from "carbon-icons-svelte/lib/ChevronUp.svelte";
+	import { slide } from "svelte/transition";
 	$: topArray = ['/images/AIPPM.jpg', '/images/CCC.jpg'];
 	$: current = '/images/DISEC.jpg';
 	$: bottomArray = ['/images/HRC.jpg', '/images/UNSC.jpg'];
+
+	function moveUp() {
+		topArray.push(current)
+		current = bottomArray[0]
+		bottomArray.shift()
+		bottomArray = bottomArray
+		topArray = topArray
+	}
+	function moveDown() {
+		bottomArray.unshift(current)
+		current = topArray[topArray.length - 1]
+		topArray.pop()
+		bottomArray = bottomArray
+		topArray = topArray
+	}
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
-	{#each topArray as topPic, i}
-		<img
-			src={topPic}
-			alt=""
-			class="absolute w-2/5 rounded-lg border-2 border-gray-500 topImage"
-			style="--margin:{(topArray.length - i)*3}rem; --scale:{0.9 - (topArray.length - i - 1)*0.15}"
-		/>
-	{/each}
-	{#each bottomArray as bottomPic, i}
-		<img
-			src={bottomPic}
-			alt=""
-			class="absolute w-2/5 rounded-lg border-2 border-gray-500 bottomImage"
-			style="--margin:{(i + 1)*3}rem; --scale:{0.9 - (i)*0.15}; --z:{5 - i}"
-		/>
-	{/each}
+	{#key topArray}
+		{#each topArray as topPic, i}
+			<img
+				src={topPic}
+				alt=""
+				transition:slide
+				class="absolute w-2/5 rounded-lg border-2 border-gray-500 topImage"
+				style="--margin:{(topArray.length - i)*3}rem; --scale:{0.9 - (topArray.length - i - 1)*0.15}"
+			/>
+		{/each}
+	{/key}
+	{#key bottomArray}
+		{#each bottomArray as bottomPic, i}
+			<img
+				src={bottomPic}
+				alt=""
+				transition:slide
+				class="absolute w-2/5 rounded-lg border-2 border-gray-500 bottomImage"
+				style="--margin:{(i + 1)*3}rem; --scale:{0.9 - (i)*0.15}; --z:{5 - i}"
+			/>
+		{/each}
+	{/key}
 	<img
 		src={current}
 		alt=""
+		transition:slide
 		class="absolute w-2/5 rounded-lg border-2 border-black transition-all hover:border-white z-10"
 	/>
+</div>
+<div class="flex flex-col text-black/50 gap-8 transition-all">
+	<ChevronUp size={24} class="hover:text-black" onclick={moveUp} />
+	<ChevronDown size={24} class="hover:text-black" onclick={moveDown} />
 </div>
 
 
