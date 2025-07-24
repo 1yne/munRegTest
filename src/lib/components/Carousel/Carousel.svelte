58ddbcc541831b8c03899e82e99a738377f395ed
1yne <tnambiar2008@gmail.com>
2025-07-24 22:45:07 +0530

    Start the committees page

---
+++ src/lib/components/Carousel/Carousel.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	$: topArray = ['/images/AIPPM.jpg', '/images/CCC.jpg'];
+	$: current = '/images/DISEC.jpg';
+	$: bottomArray = ['/images/HRC.jpg', '/images/UNSC.jpg'];
+</script>
+
+<div class="flex h-full w-full items-center justify-center">
+	{#each topArray as topPic, i}
+		<img
+			src={topPic}
+			alt=""
+			class="absolute w-2/5 rounded-lg border-2 border-gray-500 topImage"
+			style="--margin:{(topArray.length - i)*3}rem; --scale:{0.9 - (topArray.length - i - 1)*0.15}"
+		/>
+	{/each}
+	{#each bottomArray as bottomPic, i}
+		<img
+			src={bottomPic}
+			alt=""
+			class="absolute w-2/5 rounded-lg border-2 border-gray-500 bottomImage"
+			style="--margin:{(i + 1)*3}rem; --scale:{0.9 - (i)*0.15}; --z:{5 - i}"
+		/>
+	{/each}
+	<img
+		src={current}
+		alt=""
+		class="absolute w-2/5 rounded-lg border-2 border-black transition-all hover:border-white z-10"
+	/>
+</div>
+
+
+<style>
+	.topImage {
+		margin-bottom: var(--margin);
+		transform: scale(var(--scale), 1)
+	}
+	.bottomImage {
+		margin-top: var(--margin);
+		transform: scale(var(--scale), 1);
+		z-index: var(--z);
+	}
+</style>
---
+++ src/routes/committees/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Carousel from "$lib/components/Carousel/Carousel.svelte";
+</script>
+
+<div class="fixed top-0 px-12 py-6 h-full w-full">
+  <div class="w-full flex h-full">
+    <div class="w-1/2 flex flex-col items-center justify-center h-full">
+      <h1 class="font-nyghtSerif text-black text-[9rem] leading-tight">DISEC</h1>
+      <p class="text-black font-montserrat">Disarmament and International Security Council</p>
+    </div>
+    <div class="w-1/2 flex justify-center items-center h-full">
+      <Carousel />
+    </div>
+  </div>
+</div>
