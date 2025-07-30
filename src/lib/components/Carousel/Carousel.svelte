193538e59151aa325601f29ca5f2fe847b009fc0
1yne <tnambiar2008@gmail.com>
2025-07-30 18:26:46 +0530

    Move picture states to separate page

--- src/lib/components/Carousel/Carousel.svelte
+++ src/lib/components/Carousel/Carousel.svelte
@@ -2,77 +2,62 @@
 	import ChevronDown from 'carbon-icons-svelte/lib/ChevronDown.svelte';
 	import ChevronUp from 'carbon-icons-svelte/lib/ChevronUp.svelte';
 	import { fly } from 'svelte/transition';
-	import { currentPictureState } from './currentPicture.svelte';
-
-	let topArray = $state([
-		{
-			imageLink: '/images/AIPPM.jpg',
-			name: 'AIPPM',
-			full: 'All India Political Party Meet'
-		},
-		{
-			imageLink: '/images/CCC.jpg',
-			name: 'CCC',
-			full: 'Continous Crisis Committee'
-		}
-	]);
-	let bottomArray = $state([
-		{
-			imageLink: '/images/HRC.jpg',
-			name: 'HRC',
-			full: 'Human Rights Council'
-		},
-		{
-			imageLink: '/images/UNSC.jpg',
-			name: 'UNSC',
-			full: 'United Nations Security Council'
-		}
-	]);
+	import { currentPictureState, topArrayState, bottomArrayState } from './currentPicture.svelte';
+	import { goto } from '$app/navigation';
 
 	function moveUp() {
-		if (bottomArray.length != 0) {
-			topArray.push($currentPictureState);
-			$currentPictureState = bottomArray[0];
-			bottomArray.shift();
-			bottomArray = bottomArray;
-			topArray = topArray;
+		if ($bottomArrayState.length != 0) {
+			$topArrayState.push($currentPictureState);
+			$currentPictureState = $bottomArrayState[0];
+			$bottomArrayState.shift();
+			$bottomArrayState = $bottomArrayState;
+			$topArrayState = $topArrayState;
 		}
 	}
 	function moveDown() {
-		if (topArray.length != 0) {
-			bottomArray.unshift($currentPictureState);
-			$currentPictureState = topArray[topArray.length - 1]
-			topArray.pop();
-			bottomArray = bottomArray;
-			topArray = topArray;
+		if ($topArrayState.length != 0) {
+			$bottomArrayState.unshift($currentPictureState);
+			$currentPictureState = $topArrayState[$topArrayState.length - 1];
+			$topArrayState.pop();
+			$bottomArrayState = $bottomArrayState;
+			$topArrayState = $topArrayState;
 		}
 	}
 </script>
 
 <div class="flex h-full w-full items-center justify-center">
-	{#each topArray as topPic, i}
+	{#each $topArrayState as topPic, i}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 		<img
 			src={topPic.imageLink}
 			alt={topPic.name}
 			transition:fly
+			onclick={() => goto(topPic.link)}
 			class="topImage absolute w-2/5 rounded-lg border-2 border-gray-500"
-			style="--margin:{(topArray.length - i) * 3}rem; --scale:{0.9 -
-				(topArray.length - i - 1) * 0.15}"
+			style="--margin:{($topArrayState.length - i) * 3}rem; --scale:{0.9 -
+				($topArrayState.length - i - 1) * 0.15}"
 		/>
 	{/each}
-	{#each bottomArray as bottomPic, i}
+	{#each $bottomArrayState as bottomPic, i}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 		<img
 			src={bottomPic.imageLink}
 			alt={bottomPic.name}
 			transition:fly
+			onclick={() => goto(bottomPic.link)}
 			class="bottomImage absolute w-2/5 rounded-lg border-2 border-gray-500"
 			style="--margin:{(i + 1) * 3}rem; --scale:{0.9 - i * 0.15}; --z:{5 - i}"
 		/>
 	{/each}
+	<!-- svelte-ignore a11y_click_events_have_key_events -->
+	<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 	<img
 		src={$currentPictureState.imageLink}
 		alt={$currentPictureState.name}
 		transition:fly
+		onclick={() => goto($currentPictureState.link)}
 		class="absolute z-10 w-2/5 rounded-lg border-2 border-black transition-all hover:border-white"
 	/>
 </div>
--- src/lib/components/Carousel/currentPicture.svelte.ts
+++ src/lib/components/Carousel/currentPicture.svelte.ts
@@ -1,7 +1,38 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 export const currentPictureState = writable({
 	imageLink: '/images/DISEC.jpg',
 	name: 'DISEC',
-	full: 'Disarmament and International Security Meet'
-})
+	full: 'Disarmament and International Security Meet',
+	link: '/committees/disec'
+});
+
+export const topArrayState = writable([
+	{
+		imageLink: '/images/AIPPM.jpg',
+		name: 'AIPPM',
+		full: 'All India Political Party Meet',
+		link: '/committees/aippm'
+	},
+	{
+		imageLink: '/images/CCC.jpg',
+		name: 'CCC',
+		full: 'Continous Crisis Committee',
+		link: '/committees/ccc'
+	}
+]);
+
+export const bottomArrayState = writable([
+	{
+		imageLink: '/images/HRC.jpg',
+		name: 'HRC',
+		full: 'Human Rights Council',
+		link: '/committees/hrc'
+	},
+	{
+		imageLink: '/images/UNSC.jpg',
+		name: 'UNSC',
+		full: 'United Nations Security Council',
+		link: '/committees/unsc'
+	}
+]);
