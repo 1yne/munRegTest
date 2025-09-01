f81c633bfe0085c0c6ab50d1d47addfe8bfff1b3
1yne <tnambiar2008@gmail.com>
2025-09-01 17:43:43 +0530

    Remove pt-28 from file

--- src/routes/committees/+page.svelte
+++ src/routes/committees/+page.svelte
@@ -4,7 +4,7 @@
 	import { currentPictureState } from '$lib/components/Carousel/currentPicture.svelte';
 </script>
 
-<div class="fixed top-0 h-full w-full px-12 py-6 pt-28 mobile:px-6">
+<div class="fixed top-0 h-full w-full px-12 py-6 mobile:px-6">
 	<div class="flex h-full w-full mobile:hidden">
 		{#key $currentPictureState}
 			<div class="flex h-full w-1/2 flex-col items-center justify-center">
