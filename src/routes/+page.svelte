4885c808ad43794eab6f62ff8f0dc6ea3d84788f
1yne <tnambiar2008@gmail.com>
2025-09-05 22:14:56 +0530

    Update details

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@
 			<h1 class="text-[24cqw]">2025</h1>
 		</div>
 		<div class="flex w-full justify-between">
-			<h2 class="font-montserrat text-lg mobile:text-sm">25-26 October 2025</h2>
-			<h2 class="font-montserrat text-lg mobile:text-sm">RCIS, Kalyan Nagar</h2>
+			<h2 class="font-montserrat text-lg mobile:text-sm">10-11 October 2025</h2>
+			<a href="https://www.google.com/maps/place/Royale+Concorde+International+School,+Best+CBSE+School+in+Kalyan+Nagar,+Kammanahalli+,+Horamavu,+CBSE+-+Affiliation+No+830135/@13.0254756,77.6400364,17z/data=!3m1!4b1!4m6!3m5!1s0x3bae172ff41a8d9b:0x579ddbfe8e3e88e3!8m2!3d13.0254756!4d77.6426113!16s%2Fg%2F11bbwk0q3d?entry=tts&g_ep=EgoyMDI0MTAwMi4xIPu8ASoASAFQAw%3D%3D" class="font-montserrat text-lg mobile:text-sm">RCIS, Kalyan Nagar</a>
 		</div>
 	</div>
 	<div class="h-1/2 overflow-hidden bg-[url('/HeroImg.jpg')] bg-cover bg-center"></div>
